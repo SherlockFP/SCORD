d5b49182f666861bb74cc022fad6d05c8695a7e6
SherlockFP <kaankadirk31@gmail.com>
2026-05-13 19:18:34 +0300

    V5.3

--- server.py
+++ server.py
@@ -1000,10 +1000,6 @@ app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 def serve_app_js():
     return FileResponse(Path(__file__).parent / "app.js")
 
-@app.get("/fixes.js")
-def serve_fixes_js():
-    return FileResponse(Path(__file__).parent / "fixes.js")
-
 @app.get("/{full_path:path}")
 def serve_spa(full_path: str):
     return FileResponse(str(STATIC_DIR / "index.html"))
